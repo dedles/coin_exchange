f974070c00c9fe8ec8559a88d155db77a43bfe97
Chris <dedles@protonmail.com>
2018-05-05 12:53:22 -0400

    set up basic form

--- src/TradingView.js
+++ src/TradingView.js
@@ -11,14 +11,63 @@ class TradingView extends React.Component {
   constructor(){
     super()
     this.state = {
-
+      currencyFrom: 'USD',
+      currencyTo: "BTC",
+      tradeAmount: ""
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange(value, stateItem) {
+    this.setState({
+      [stateItem]: value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    debugger;
+  }
+
+
   render() {
+    let {usdBalance, bitcoinBalance} = this.props;
+    let {currencyFrom, currencyTo, tradeAmount } = this.state;
     return (
-      <div className="viewContainer">
-        TradingView
+      <div className="root">
+        <div className="viewContainer">
+          Account Balance
+          <br/>
+          <p>
+            <span className="currencyType">USD</span> {usdBalance}
+          </p>
+          <p>
+            <span className="currencyType">BITCOIN</span> {bitcoinBalance}
+          </p>
+
+          <form onSubmit={this.handleSubmit} className="tradeForm">
+            Trade
+            <select value={currencyFrom} onChange={(e) => this.handleChange(e.target.value, "currencyFrom")}>
+              <option value="USD">USD</option>
+              <option value="BTC">BTC</option>
+            </select>
+            <input type="text" placeholder="Enter your amount" value={tradeAmount} onChange={(e) => this.handleChange(e.target.value, 'tradeAmount')}/>
+            <select value={currencyTo} onChange={(e) => this.handleChange(e.target.value, "currencyFrom")}>
+              <option value="USD">USD</option>
+              <option value="BTC">BTC</option>
+            </select>
+            <div className="quote">
+              asdf
+            </div>
+            <button type="submit" className="submit">
+              Trade
+            </button>
+
+          </form>
+        </div>
+
       </div>
     )
   }
